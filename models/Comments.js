d9f3ac715b7f1fb0bd5a0ed3cbff9595ac34f713
Jack Spence <jackspence123456@gmail.com>
2023-11-16 14:28:53 +0200

    Added properties to Comments model.

---
+++ models/Comments.js
@@ -0,0 +1,59 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comments extends Model {}
+
+// Added Properties to Comments Model
+Comments.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		content: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			unique: true,
+			references: {
+				model: "posts",
+				key: "id",
+			},
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			unique: true,
+			references: {
+				model: "users",
+				key: "id",
+			},
+		},
+		date_created: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+		date_updated: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "comments",
+	}
+);
+
+// Exports
+module.exports = Comments;
