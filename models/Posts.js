33c15ddf92560dce72c001c82cddea4eb161be59
Jack Spence <jackspence123456@gmail.com>
2023-11-16 14:25:37 +0200

    Added Posts properties to Posts model.

---
+++ models/Posts.js
@@ -0,0 +1,55 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model {}
+
+// Added Properties to Posts Model
+Posts.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		content: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			unique: true,
+			references: {
+				model: "Users",
+				key: "id",
+			},
+		},
+		date_created: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+		date_updated: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "posts",
+	}
+);
+
+// Exports
+module.exports = Posts;
