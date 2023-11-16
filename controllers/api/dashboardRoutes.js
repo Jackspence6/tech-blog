b2b4c14b9ccdacb4a61c5044239eef22c8a26e97
Jack Spence <jackspence123456@gmail.com>
2023-11-16 16:00:11 +0200

    Added dashboard route.

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,6 @@
+// Imports & supporting NPM modules
+const router = require("express").Router();
+const { Blogs, Comments, Users } = require("../../models");
+
+// Exports
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,13 @@ const router = require("express").Router();
 const blogRoutes = require("./blogRoutes");
 const commentRoutes = require("./commentRoutes");
 const userRoutes = require("./userRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 // Middleware
 router.use("./blogs", blogRoutes);
 router.use("/comments", commentRoutes);
 router.use("/users", userRoutes);
+router.use("/dashboards", dashboardRoutes);
 
 // Exports
 module.exports = router;
