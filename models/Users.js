319b41d265dd6e759ab6b5c9ef0dc3e8a8333e37
Jack Spence <jackspence123456@gmail.com>
2023-11-16 14:25:01 +0200

    Added Users properties to model.

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Users extends Model {
+	// Created method to checkPassword
+	checkPassword(loginPW) {
+		return bcrypt.compareSync(loginPW, this.password);
+	}
+}
+
+// Added Properties to Users Model
+Users.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				min: 8,
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "users",
+	}
+);
+
+// Exports
+module.exports = Users;
