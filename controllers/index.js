87ce15612d3c4191e2589dda22154b259db2c565
Jack Spence <jackspence123456@gmail.com>
2023-11-17 14:36:01 +0200

    Added router middleware for homeRoutes.

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 // Imports & supporting NPM modules
 const router = require("express").Router();
+
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
 // Turning on router
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 // Exports
