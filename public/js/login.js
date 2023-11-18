abe4cd2f40667f4f36c3217d37b3a9500837d734
Jack Spence <jackspence123456@gmail.com>
2023-11-18 19:01:33 +0200

    Added signup form handler to login.js.

--- public/js/login.js
+++ public/js/login.js
@@ -23,6 +23,34 @@ const loginFormHandler = async (event) => {
 	}
 };
 
+// Signup Form Handler
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	// Collecting values from the signup form
+	const username = document.querySelector("#username-signup").value.trim();
+	const password = document.querySelector("#password-signup").value.trim();
+
+	if (username && password) {
+		// Sending a POST request to the API endpoint
+		const response = await fetch("/api/users", {
+			method: "POST",
+			body: JSON.stringify({ username, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+		// If successful, redirecting user to homepage
+		if (response.ok) {
+			document.location.replace("/");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
 document
 	.querySelector(".login-form")
 	.addEventListener("submit", loginFormHandler);
+
+document
+	.querySelector(".signup-form")
+	.addEventListener("submit", signupFormHandler);
