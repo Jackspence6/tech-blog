fa937608c08371f1999da636d1187723b2d22871
Jack Spence <jackspence123456@gmail.com>
2023-11-19 12:31:58 +0200

    Added route to redirect user to login/signup if not logged in.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 // Imports & supporting NPM modules
 const router = require("express").Router();
 const { Blogs, Comments, Users } = require("../models");
+const { route } = require("./home-routes");
 
 // Route to get all Blogs along with their associated Comments and Users
 router.get("/", async (req, res) => {
@@ -35,6 +36,19 @@ router.get("/", async (req, res) => {
 		res
 			.status(500)
 			.render("errorPage", { message: "❌Error fetching blog posts!" });
+	}
+});
+
+// Route to redirect user to login page if not already logged in
+router.get("/login", async (req, res) => {
+	try {
+		if (req.session.logged_in) {
+			res.redirect("/dashboards");
+			return;
+		}
+		res.render("login");
+	} catch (err) {
+		res.status(400).json(err);
 	}
 });
 
