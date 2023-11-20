566ec8f573f9958c627399ba07d62ee28795617b
Jack Spence <jackspence123456@gmail.com>
2023-11-20 17:11:51 +0200

    Added Route to add comment to blog post.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,5 +2,24 @@
 const router = require("express").Router();
 const { Comments } = require("../../models");
 
+// Route to add comment to blog post
+router.post("/", async (req, res) => {
+	if (!req.session.userId) {
+		return res.status(401).send("You must be logged in to comment!");
+	}
+
+	try {
+		await Comments.create({
+			content: req.body.content,
+			postId: req.body.blog_id,
+			userId: req.session.user_id,
+		});
+
+		res.redirect(`/posts/${req.body.blog_id}`);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
