c8dd5ef8f9519451dd64b01e172f2b82f658312e
Jack Spence <jackspence123456@gmail.com>
2023-11-20 15:29:51 +0200

    Added Function to handle add comment form.

--- public/js/blogs.js
+++ public/js/blogs.js
@@ -17,3 +17,22 @@ var viewBlogButtons = document.querySelectorAll(".viewBlogBtn");
 viewBlogButtons.forEach((button) => {
 	button.addEventListener("click", openBlog);
 });
+
+// Function to handle add comment form
+function addComment(event) {
+	event.preventDefault();
+
+	var blogId = event.target.getAttribute("data-blog-id");
+
+	if (blogId) {
+		// Redirecting user to the add comment form
+		window.location.href = "/comments/";
+	} else {
+		console.error("An error occurred!");
+	}
+}
+
+// Adding an event listener to each add comment btn
+var addCommentBtn = document.querySelector(".addCommentBtn");
+
+addCommentBtn.addEventListener("click", addComment);
