5940c06cb84266ff0c19e97309f7342df66f2af7
Jack Spence <jackspence123456@gmail.com>
2023-11-20 20:59:59 +0200

    Added Route to delete individual comment by id.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,7 @@
 // Imports & supporting NPM modules
 const router = require("express").Router();
-const { Comments } = require("../../models");
+const { Comments, Users } = require("../../models");
+const { findByPk } = require("../../models/Blogs");
 
 // Route to add comment to blog post
 router.post("/", async (req, res) => {
@@ -10,8 +11,30 @@ router.post("/", async (req, res) => {
 
 	try {
 		const commentData = await Comments.create({
-			...req.body,
+			content: Comments.content,
+			user_id: Comments.user.user_id,
+			date_created: Comments.date_created,
 		});
+
+		res.status(200).json(commentData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// Route to delete individual comment by id
+router.delete("/:id", async (req, res) => {
+	try {
+		const commentData = await Comments.destroy(req.params.id, {
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (!commentData) {
+			res.status(404).json({ message: "No comment found with this ID!" });
+			return;
+		}
 
 		res.status(200).json(commentData);
 	} catch (err) {
