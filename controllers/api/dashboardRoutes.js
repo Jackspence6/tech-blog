6155ef3ea286c8d7609ea34a0f1104b77b0c938b
Jack Spence <jackspence123456@gmail.com>
2023-11-21 14:05:46 +0200

    Added Route to get user dashboard.

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -2,5 +2,35 @@
 const router = require("express").Router();
 const { Blogs, Comments, Users } = require("../../models");
 
+// Route to get user dashboard
+router.get("/", async (req, res) => {
+	try {
+		const user_id = req.session.user_id;
+
+		if (!user_id) {
+			res.status(401).json({ message: "User not authenticated" });
+			return;
+		}
+
+		const dashboardData = await Blogs.findAll({
+			where: { user_id: user_id },
+		});
+
+		if (!dashboardData) {
+			res.status(400).json({ message: "No Blogs found matching your Id!" });
+			return;
+		}
+
+		// Serializing data so the template can read it
+		const dashboard = dashboardData.map((dashboard) =>
+			dashboard.get({ plain: true })
+		);
+
+		res.render("homepage", { dashboard });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
