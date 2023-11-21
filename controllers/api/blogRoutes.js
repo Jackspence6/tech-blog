0664cf980c2810a65facc1f558c6d1347e210031
Jack Spence <jackspence123456@gmail.com>
2023-11-21 18:39:07 +0200

    Added Route to create a new Blog Post.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,5 +2,23 @@
 const router = require("express").Router();
 const { Blogs } = require("../../models");
 
+// Route to create a new Blog Post
+router.post("/", async (req, res) => {
+	try {
+		const { title, content } = req.body;
+		const user_id = req.session.user_id;
+
+		const blogData = await Blogs.create({
+			title,
+			content,
+			user_id,
+		});
+
+		res.status(200).json(blogData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
