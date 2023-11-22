e221a4a0bda99898ac0cfc7790850146af200b60
Jack Spence <jackspence123456@gmail.com>
2023-11-22 21:17:13 +0200

    Added Route to update an existing Blog Post.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -25,5 +25,39 @@ router.post("/", async (req, res) => {
 	}
 });
 
+// Route to update an existing Blog Post
+router.put("/:id", async (req, res) => {
+	try {
+		const { title, content } = req.body;
+		const user_id = req.session.user_id;
+		const blogId = req.params.id;
+
+		// Authentication Check
+		if (!user_id) {
+			res.status(401).json({ message: "User not authenticated!" });
+			return;
+		}
+
+		// Finding blog post by ID
+		const blogData = await Blogs.findByPk(blogId);
+
+		// Checking if blog post exists and belongs to the user
+		if (!blogData || blogData.user_id !== user_id) {
+			res.status(404).json({ message: "Blog post not found or unauthorized!" });
+			return;
+		}
+
+		// Updating blog post
+		await blogData.update({
+			title: title || blogData.title,
+			content: content || blogData.content,
+		});
+
+		res.status(200).json({ message: "Blog post updated successfully!" });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
